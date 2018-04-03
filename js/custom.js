bb85c52aa265acc717763c3c218604e8de174498
Lia5 <Liaforweb@gmail.com>
2018-04-03 23:14:09 +0300

    user-cab-ist

--- js/custom.js
+++ js/custom.js
@@ -267,12 +267,18 @@ counter
       }
   });
 
-/*maskedinput*/
+
+/*===================
+maskedinput
+===================*/
 $(function(){
   $(".phone-mask").mask("(999) 999-99-99");
 })
 
-/*datepicker*/
+
+/*===================
+datepicker
+===================*/
 $("#page-order-date").datepicker({
   dateFormat:"DD, dd.mm"
 });
@@ -280,3 +286,55 @@ $("#page-order-time").datepicker({
   // multipleDates: 2,
   timepicker: true
 });
+
+
+/*===================
+tab-filter
+===================*/
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("user-cab-tab-list");
+var btns = btnContainer.getElementsByClassName("user-cab-tab-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
